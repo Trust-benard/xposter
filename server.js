868fbc42bd3c7f27a2e38284e0b0a76f231d2d52
Trust Benard <frankcastleexx@gmail.com>
2025-06-05 13:31:49 +0100

    Convert to web service for Render free tier

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { postTweet } = require('./poster');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(express.json());
